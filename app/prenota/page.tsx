28f8844d39dfe3fccd7f81cf3914188ed7977268
Mattia Marcon <149693753+mattiamarcon@users.noreply.github.com>
2026-06-19 10:56:27 +0200

    update calendar link

--- app/prenota/page.tsx
+++ app/prenota/page.tsx
@@ -1,7 +1,7 @@
 export default function BookingPage() {
   return (
     <iframe
-                src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ3Q6M14oyztZaSOglbq9uxXvz0C2QS8qfOxMhZCpe9-fK9iK5Ds9SP_E1PFc5_wtnsuMQBqi1ji? gv=true"
+                src="https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ1w7gxPIoaC-q1JToTvShmrEIFwU--9kikR07vunqC2-7usd15Zb9ntpIQtmHA0RHQEvT8SCw_B"
                 className="h-[500px] w-full md:h-[600px] lg:h-[700px]"
                 title="Calendario prenotazioni"
               />
